1269f77620f9b9a174518a115b3595bee4c5d9dc
Seun Amodeni <96447667+specs-lanre@users.noreply.github.com>
2023-09-15 08:55:45 -0700

    flask_app_webfiles
    flask_app_webfiles

---
+++ js/title.js
@@ -0,0 +1,16 @@
+//alert("from external js file")
+//document.getElementById('abouttitle').innerHTML="<div class='fs-3 my-3'>About RealTime Adds</div>"
+//document.getElementById('contacttitle').innerHTML="<div class='fs-3 my-3'>Contact us at  RealTime Adds</div>"
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/websitejsfile.js
@@ -0,0 +1,35 @@
+//alert("from external js file")
+/*
+a=100
+b=25
+c=100-25
+result=a+" - "+b+" = "+c
+console.log(c)
+document.getElementById('xmark').innerHTML=result
+*/
+
+aval=234
+Aval=400
+
+aval/2
+
+Aval/2
+
+document.write("<button class='btn btn-success'>Click here</button>")
+
+document.write("<h2>we are learning javascript</h2>")
+
+document.write("<h2>it is fun fun fun</h2>")
+
+window.open("about.html")
+window.open("https://www.google.com")
+
+
+
+window.open("request.html","",specs="width=500px,height=500px");
+
+window.open("","",specs="width=500px,height=500px");
+
+
+
+
